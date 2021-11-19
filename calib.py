d4ecc1b112bae28f4359fc6457605ec5202e0113
Niklas Drössler <n-droessler@outlook.de>
2021-11-19 19:17:13 +0100

    .

--- calib.py
+++ calib.py
@@ -2,43 +2,80 @@ import numpy as np
 import cv2 as cv
 import glob
 
-grid_x = 6
-grid_y = 7
+DEBUG = False
 
-# termination criteria
-criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(grid_x,5,0)
-objp = np.zeros((grid_x * grid_y, 3), np.float32)
-objp[:, :2] = np.mgrid[0:grid_y, 0:grid_x].T.reshape(-1, 2)
 
-# Arrays to store object points and image points from all the images.
-objpoints = []  # 3d point in real world space
-imgpoints = []  # 2d points in image plane.
-images = glob.glob('resources/Udacity/calib/*.jpg')
+class CameraCalibration:
+    def __init__(self):
+        # configuration of grid size
+        self.grid_x = 6
+        self.grid_y = 9
 
-for fname in images:
-    print(f"Evaluating: {fname}")
+        # termination criteria
+        self.criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-    img = cv.imread(fname, cv.IMREAD_GRAYSCALE)
-    thresh_img = cv.threshold(img, thresh=10, maxval=255, type=cv.THRESH_BINARY)
-    cv.imshow('testt', thresh_img)
-    # Find the chess board corners
-    ret, corners = cv.findChessboardCorners(np.float32(thresh_img), (grid_y, grid_x), None)
+        # Arrays to store object points and image points from all the images.
+        self.objpoints = []  # 3d point in real world space
+        self.imgpoints = []  #
 
-    # If found, add object points, image points (after refining them)
-    if ret:
-        objpoints.append(objp)
+        # todo: out
+        self.images = glob.glob('resources/Udacity/calib/*.jpg')
 
-        corners2 = cv.cornerSubPix(thresh_img, corners, (11, 11), (-1, -1), criteria)
-        imgpoints.append(corners)
+    def calibrate(self, image_paths):
+        images = image_paths
 
-        # Draw and display the corners
-        cv.drawChessboardCorners(img, (grid_y, grid_x), corners2, ret)
-        cv.imshow('img', img)
-        cv.waitKey(10)
-        print("\t--> good")
-    else:
-        print("\t--> faulty")
+        # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(grid_x,5,0)
+        objp = np.zeros((self.grid_x * self.grid_y, 3), np.float32)
+        objp[:, :2] = np.mgrid[0:self.grid_y, 0:self.grid_x].T.reshape(-1, 2)
+
+        for fname in self.images:
+            if DEBUG:
+                print(f"Evaluating: {fname}")
+
+            img = cv.imread(fname, cv.IMREAD_GRAYSCALE)
+            # Find the chess board corners
+            ret, corners = cv.findChessboardCorners(img, (self.grid_y, self.grid_x), None)
+
+            # If found, add object points, image points (after refining them)
+            if ret:
+                self.objpoints.append(objp)
+
+                corners2 = cv.cornerSubPix(img, corners, (11, 11), (-1, -1), self.criteria)
+                self.imgpoints.append(corners)
+
+                # Draw and display the corners
+                if DEBUG:
+                    cv.drawChessboardCorners(img, (self.grid_y, self.grid_x), corners2, ret)
+                    cv.imshow('img', img)
+                    cv.waitKey(10)
+                    print("\t--> good")
+            elif DEBUG:
+                print("\t--> faulty")
+
+print(len(imgpoints))
+
+# calibration
+ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, img.shape[::-1], None, None)
+
+# Undistortion
+img = cv.imread('resources/Udacity/calib/calibration3.jpg')
+h, w = img.shape[:2]
+newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+
+# undistort
+dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+
+# cropping
+x, y, w, h = roi
+dst = dst[y:y + h, x:x + w]
+
+# show tests
+cv.imshow("distorted", img)
+cv.waitKey(5000)
+
+# show tests
+cv.imshow("Undistort", dst)
+cv.waitKey(5000)
 
 cv.destroyAllWindows()
 
